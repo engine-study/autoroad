a8f2dbd1183ef0499520760b8d7f5bde0c70783f
Neilson K-S <neilson.ks@gmail.com>
2023-06-27 11:49:17 -0700

    game works again, movement etc.

---
+++ packages/contracts/src/systems/GridSystem.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import { System } from "@latticexyz/world/src/System.sol";
+import { MapConfig, Damage, Position, PositionTableId, Player, PositionData, Health } from "../codegen/Tables.sol";
+import { getKeysWithValue } from "@latticexyz/world/src/modules/keyswithvalue/getKeysWithValue.sol";
+import { addressToEntityKey } from "../addressToEntityKey.sol";
+
+contract MoveSystem is System {
+
+    
+
+}
