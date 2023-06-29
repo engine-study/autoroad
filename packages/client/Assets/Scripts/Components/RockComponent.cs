fae2e630ef5cfee8d0a693f81d70a12bd9be7c4e
Neilson K-S <neilson.ks@gmail.com>
2023-06-29 15:14:26 -0700

    mining cont.

--- packages/client/Assets/Scripts/Components/RockComponent.cs
+++ packages/client/Assets/Scripts/Components/RockComponent.cs
@@ -10,15 +10,25 @@ public class RockComponent : MUDComponent
     public int Stage { get { return stage; } }
 
     [Header("Rock")]
-    [SerializeField] protected int stage;
+    [SerializeField] protected int stage = -1;
     [SerializeField] protected RockType rockType;
+    [SerializeField] SPAudioSource source;
 
     [SerializeField] GameObject[] stages;
+    [SerializeField] AudioClip [] sfx_break;
+
+    protected override void Awake() {
+        base.Awake();
+
+        stage = -1;
+    }
 
     protected override void UpdateComponent(mud.Client.IMudTable update, UpdateEvent eventType)
     {
 
         base.UpdateComponent(update, eventType);
+
+        int lastStage = stage;
 
         RockTable rockUpdate = (RockTable)update;
 
@@ -33,14 +43,20 @@ public class RockComponent : MUDComponent
             stage = (int)rockUpdate.size;
             rockType = (RockType)rockUpdate.rockType;
 
-
-
         }
 
-        for (int i = 0; i < stages.Length; i++)
-        {
-            stages[i].SetActive(i == stage);
+        if(lastStage != -1) {
+
+            source.PlaySound(sfx_break);
+            
+            for (int i = 0; i < stages.Length; i++)
+            {
+                stages[i].SetActive(i == stage);
+            
+            }
+        
         }
+
     }
 
 }
