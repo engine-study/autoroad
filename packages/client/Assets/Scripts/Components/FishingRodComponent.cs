b68e15625331522a1cb9a879223496160cc8a1d7
Neilson <neilson.ks@gmail.com>
2023-10-20 13:59:40 -0700

    (warning) player reserialized, plant animation

--- packages/client/Assets/Scripts/Components/FishingRodComponent.cs
+++ packages/client/Assets/Scripts/Components/FishingRodComponent.cs
@@ -5,10 +5,6 @@ using mud;
 public class FishingRodComponent : PropComponent {
  
     protected override IMudTable GetTable() {return new FishingRodTable();}
-    protected override void UpdateComponent(IMudTable update, UpdateInfo newInfo) {
-        FishingRodTable table = update as FishingRodTable;
-    }
-
     protected override StatType SetStat(IMudTable mudTable) {return StatType.None;}
 
 }
--- packages/client/Assets/Scripts/Components/StickComponent.cs
+++ packages/client/Assets/Scripts/Components/StickComponent.cs
@@ -5,8 +5,4 @@ using mud;
 public class StickComponent : PropComponent {
 
     protected override IMudTable GetTable() {return new StickTable();}
-    protected override void UpdateComponent(IMudTable update, UpdateInfo newInfo) {
-        StickTable table = update as StickTable;
-    }
-
 }
